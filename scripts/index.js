a71fc41eb7db63ea133b5b9b20579c5908dd73e6
Bayarto Divasanov <bayarto.divasanov@gmail.com>
2024-01-29 21:01:04 -0500

    1 коммит

--- scripts/index.js
+++ scripts/index.js
@@ -1,9 +1,26 @@
 // @todo: Темплейт карточки
-
+   const cardTemplate = document.getElementById('card-template').content.querySelector('.card');
 // @todo: DOM узлы
-
+    const cardList = document.querySelector('.places__list');
 // @todo: Функция создания карточки
+    function createCard (dataCard, deleteCard) {
+        const card = cardTemplate.cloneNode(true);
+        const title = card.querySelector('.card__title');
+        const image= card.querySelector('.card__image');
+        const deleteButton = card.querySelector('.card__delete-button');
 
+        title.textContent = dataCard.name;
+        image.setAttribute('src', dataCard.link);
+        
+        deleteButton.addEventListener('click', deleteButton);
+
+        return card;
+    }
 // @todo: Функция удаления карточки
-
+    function deleteCard (card) {
+        card.parentElement.remove()
+    }
 // @todo: Вывести карточки на страницу
+    initialCards.forEach(function(card) {
+        cardList.append(createCard(card, deleteCard))
+    })
